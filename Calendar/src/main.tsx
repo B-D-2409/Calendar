6e0a9f83258794eea6e45754c107800c803deabe
Masterclass777 <borislav.2404g@gmail.com>
2025-07-10 15:50:16 +0300

    deploy front end file

--- Calendar/src/main.tsx
+++ Calendar/src/main.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './App'
 import { BrowserRouter } from 'react-router-dom'
-import { ThemeProvider } from './Components/ThemeProvider/ThemeProvider.tsx'
-import  AuthProvider  from "./Common/AuthContext.tsx";
+import { ThemeProvider } from './Components/ThemeProvider/ThemeProvider'
+import  AuthProvider  from "./Common/AuthContext";
 createRoot(document.getElementById('root')!).render(
   <AuthProvider>
   <BrowserRouter>
